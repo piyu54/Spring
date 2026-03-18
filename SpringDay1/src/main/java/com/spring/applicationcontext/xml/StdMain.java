01ae8875e31d1dd39684ddb0df5b165ebd46ba20
unknown <priyakushwaha1407@gmail.com>
2026-03-18 22:14:59 +0530

    commit

--- SpringDay1/src/main/java/com/spring/applicationcontext/xml/StdMain.java
+++ SpringDay1/src/main/java/com/spring/applicationcontext/xml/StdMain.java
@@ -7,7 +7,7 @@ public class StdMain {
 
 	
 	public static void main(String[] args) {
-		//
+		
 		ApplicationContext context=new ClassPathXmlApplicationContext("springbeans.xml");
 		Student s=context.getBean("std",Student.class);
 		System.out.println(s);
