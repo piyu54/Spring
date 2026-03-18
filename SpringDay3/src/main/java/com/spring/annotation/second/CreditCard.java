174654156324330d3848abcef2d20a15d55354ca
unknown <priyakushwaha1407@gmail.com>
2026-03-18 22:01:22 +0530

    commit

--- SpringDay3/src/main/java/com/spring/annotation/second/CreditCard.java
+++ SpringDay3/src/main/java/com/spring/annotation/second/CreditCard.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component("credit")
 public class CreditCard implements Payment{
-//
+
 	@Override
 	public String Pay() {
 		return "Payment done using Credit Card";
