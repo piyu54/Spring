a190e8836ed3cd9ae0ef0eaa7b2d4ad70a920ef4
unknown <priyakushwaha1407@gmail.com>
2026-03-18 21:56:51 +0530

    commit

--- SpringDay4/src/main/java/com/aspectdemo/MainClass.java
+++ SpringDay4/src/main/java/com/aspectdemo/MainClass.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class MainClass {
 	public static void main(String[] args) {
 
-		
+		//
 		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		Recharge e=context.getBean(Recharge.class);
 		e.myRecharge();
